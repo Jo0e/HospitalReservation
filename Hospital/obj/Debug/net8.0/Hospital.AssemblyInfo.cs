8621fd552e815a7f046cdbd218014b5f709ee9b1
Jo0e <ykj627@gmail.com>
2024-10-01 16:52:34 +0300

    commit

--- Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
+++ Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b46c01992aecd1f56d03192ca998c5b05bc0818")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
